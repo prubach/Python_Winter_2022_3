69ccaf3523d6982c9b887439d87d97dc4cd6957c
prubac <pawel.rubach@sgh.waw.pl>
2022-10-27 14:01:20 +0200

    Comment to demo git

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    # New comment to demo git
+    print('Hello from git')
 
 
 
