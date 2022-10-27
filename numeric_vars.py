2c44dd54e4d624684fabf6c272634f41e6e9e15d
prubac <pawel.rubach@sgh.waw.pl>
2022-10-27 14:49:56 +0200

    Numeric vars

---
+++ numeric_vars.py
@@ -0,0 +1,47 @@
+a = 376725756827616713562362341523142142352345636235624524
+b = a + 1
+print(a)
+print(b)
+f = 376725756827616713562362341523142142352345636235624524.5
+print(f)
+g = f + 1000000
+print(g)
+h = 0.0000000000000000000000224242
+print(h)
+print(type(h))
+print(type(b))
+i = 1.79e308
+j = 1.8e308
+print(i)
+print(j)
+print('-----------')
+
+bb = 0b101
+print(bb)
+print('-----------')
+hx = 0x1a
+print(hx)
+
+print('-----------')
+print(2**8)
+# rest of division
+print(19 % 9)
+a = 200
+b = 5
+c = a / b
+print(c)
+print(type(c))
+c = 50.6336
+print(int(c))
+print(round(c))
+print(round(c, 2))
+s1 = '64'
+s2 = '25.5425'
+# Test if is an int number
+print(s1.isnumeric())
+ee = int(s1) + float(s2)
+print(ee)
+
+
+
+
