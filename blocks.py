17c697b6a291ec994e8afb7148fdfe84bf31d71f
prubac <pawel.rubach@sgh.waw.pl>
2022-11-10 14:18:19 +0100

    Blocks - variables visibility

---
+++ blocks.py
@@ -0,0 +1,15 @@
+a = 3
+for i in range(4):
+    b = 5
+    print(f'i={i}')
+    print(f'a={a}')
+print('--------')
+print(f'b={b}')
+
+if a > 4:
+    if b > 6:
+        c = 50
+    else:
+        c = 10
+
+print(f'c={c}')
