c6dd4d44aebbb25be6ca7eb69f8131f6a61976d9
prubac <pawel.rubach@sgh.waw.pl>
2022-11-24 14:19:38 +0100

    Read csv file and parse it

---
+++ file_read_csv.py
@@ -0,0 +1,17 @@
+my_file = 'my_matrix.csv'
+
+m = []
+with open(my_file, 'r') as f:
+    i = 0
+    while line:=f.readline():
+        r = []
+        i += 1
+        print(f'{i}: {line}', end='')
+        for c in line.split(';'):
+            print(c)
+            r.append(int(c))
+            #print(type(c))
+        m.append(r)
+
+print(m)
+print(type(m[0][1]))
