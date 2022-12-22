888f54ccb9287003eaa9c82cf84f135a777cff4d
prubac <pawel.rubach@sgh.waw.pl>
2022-12-22 14:47:31 +0100

    Banking app - account

--- bank.py
+++ bank.py
@@ -11,5 +11,20 @@ class Customer:
         return f'Customer[{self.id}, {self.firstname}, {self.lastname}]'
 
 
+class Account:
+    last_id = 1000
+
+    def __init__(self, customer):
+        self.customer = customer
+        Account.last_id += 1
+        self.id = Account.last_id
+        self._balance = 0
+
+    def __repr__(self):
+        return f'Account[{self.id}, {self.customer.lastname}, {self._balance}]'
+
+
 c = Customer('John', 'Brown')
 print(c)
+a = Account(c)
+print(a)
